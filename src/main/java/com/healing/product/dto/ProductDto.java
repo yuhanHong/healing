ea096718973c5e75450eb30c24477c398278be36
yuhan <ghddbgks@naver.com>
2015-12-07 19:57:13 +0900

    2015_12_07 홍유한 ProductDto입니다

--- src/main/java/com/healing/product/dto/ProductDto.java
+++ src/main/java/com/healing/product/dto/ProductDto.java
@@ -1,5 +1,116 @@
 package com.healing.product.dto;
 
 public class ProductDto {
-
+	private int product_number;
+	private String pruduct_name;
+	private String product_category;
+	private String product_country;
+	private int product_stay_date;
+	private int product_price_adult;
+	private int product_price_child;
+	private int product_price_baby;
+	private int product_size;
+	private String product_summary;
+	private int product_readcount;
+	private int product_average_score;
+	private int product_bargain_rate1;
+	private int product_bargain_day1;
+	private int product_bargain_rate2;
+	private int product_bargain_day2;
+	public int getProduct_number() {
+		return product_number;
+	}
+	public void setProduct_number(int product_number) {
+		this.product_number = product_number;
+	}
+	public String getPruduct_name() {
+		return pruduct_name;
+	}
+	public void setPruduct_name(String pruduct_name) {
+		this.pruduct_name = pruduct_name;
+	}
+	public String getProduct_category() {
+		return product_category;
+	}
+	public void setProduct_category(String product_category) {
+		this.product_category = product_category;
+	}
+	public String getProduct_country() {
+		return product_country;
+	}
+	public void setProduct_country(String product_country) {
+		this.product_country = product_country;
+	}
+	public int getProduct_stay_date() {
+		return product_stay_date;
+	}
+	public void setProduct_stay_date(int product_stay_date) {
+		this.product_stay_date = product_stay_date;
+	}
+	public int getProduct_price_adult() {
+		return product_price_adult;
+	}
+	public void setProduct_price_adult(int product_price_adult) {
+		this.product_price_adult = product_price_adult;
+	}
+	public int getProduct_price_child() {
+		return product_price_child;
+	}
+	public void setProduct_price_child(int product_price_child) {
+		this.product_price_child = product_price_child;
+	}
+	public int getProduct_price_baby() {
+		return product_price_baby;
+	}
+	public void setProduct_price_baby(int product_price_baby) {
+		this.product_price_baby = product_price_baby;
+	}
+	public int getProduct_size() {
+		return product_size;
+	}
+	public void setProduct_size(int product_size) {
+		this.product_size = product_size;
+	}
+	public String getProduct_summary() {
+		return product_summary;
+	}
+	public void setProduct_summary(String product_summary) {
+		this.product_summary = product_summary;
+	}
+	public int getProduct_readcount() {
+		return product_readcount;
+	}
+	public void setProduct_readcount(int product_readcount) {
+		this.product_readcount = product_readcount;
+	}
+	public int getProduct_average_score() {
+		return product_average_score;
+	}
+	public void setProduct_average_score(int product_average_score) {
+		this.product_average_score = product_average_score;
+	}
+	public int getProduct_bargain_rate1() {
+		return product_bargain_rate1;
+	}
+	public void setProduct_bargain_rate1(int product_bargain_rate1) {
+		this.product_bargain_rate1 = product_bargain_rate1;
+	}
+	public int getProduct_bargain_day1() {
+		return product_bargain_day1;
+	}
+	public void setProduct_bargain_day1(int product_bargain_day1) {
+		this.product_bargain_day1 = product_bargain_day1;
+	}
+	public int getProduct_bargain_rate2() {
+		return product_bargain_rate2;
+	}
+	public void setProduct_bargain_rate2(int product_bargain_rate2) {
+		this.product_bargain_rate2 = product_bargain_rate2;
+	}
+	public int getProduct_bargain_day2() {
+		return product_bargain_day2;
+	}
+	public void setProduct_bargain_day2(int product_bargain_day2) {
+		this.product_bargain_day2 = product_bargain_day2;
+	}
 }
