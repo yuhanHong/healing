4f1ab220623e1e37ca25782dd8eae82cfd81c0fa
chaeyoon <susie2380@naver.com>
2015-12-11 19:53:46 +0900

    2015_12_11 common.js 공통 자바스크립트 함수 v0.1

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -3,6 +3,21 @@
  */
 
 // 숫자에 쉼표 찍어주는 함수 (int -> String)
+function numberFormatter(result){
+	var len=(result+"").length;
+	var rest=len%3;
+	
+	for(var i=0;i<len/3-1;i++){
+		result=interceptString(result,rest+i*4);
+	}
+	
+	return result;
+}
+
+function interceptString(str,index){
+	str=str+"";
+	return str.substring(0,index)+","+str.substring(index,str.length);
+}
 
 // timeStamp를 yy-mm-dd로 바꿔주는 함수 (timeStamp -> date)
 
