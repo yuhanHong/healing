d20928d1f0b1bebc4ee42383dd43756bc718d1ea
yuhan <ghddbgks@naver.com>
2015-12-14 12:49:47 +0900

    2015_12_14 홍유한 ProductDayDto 올립니다~

---
+++ src/main/java/com/healing/product/dto/ProductDayDto.java
@@ -0,0 +1,54 @@
+package com.healing.product.dto;
+
+public class ProductDay {
+	private int product_day_number;
+	private int product_number;
+	private int product_day_nth_day;
+	private String product_day_breakfast;
+	private String product_day_lunch;
+	private String product_day_dinner;
+	private String product_day_hotel;
+	
+	public int getProduct_day_number() {
+		return product_day_number;
+	}
+	public void setProduct_day_number(int product_day_number) {
+		this.product_day_number = product_day_number;
+	}
+	public int getProduct_number() {
+		return product_number;
+	}
+	public void setProduct_number(int product_number) {
+		this.product_number = product_number;
+	}
+	public int getProduct_day_nth_day() {
+		return product_day_nth_day;
+	}
+	public void setProduct_day_nth_day(int product_day_nth_day) {
+		this.product_day_nth_day = product_day_nth_day;
+	}
+	public String getProduct_day_breakfast() {
+		return product_day_breakfast;
+	}
+	public void setProduct_day_breakfast(String product_day_breakfast) {
+		this.product_day_breakfast = product_day_breakfast;
+	}
+	public String getProduct_day_lunch() {
+		return product_day_lunch;
+	}
+	public void setProduct_day_lunch(String product_day_lunch) {
+		this.product_day_lunch = product_day_lunch;
+	}
+	public String getProduct_day_dinner() {
+		return product_day_dinner;
+	}
+	public void setProduct_day_dinner(String product_day_dinner) {
+		this.product_day_dinner = product_day_dinner;
+	}
+	public String getProduct_day_hotel() {
+		return product_day_hotel;
+	}
+	public void setProduct_day_hotel(String product_day_hotel) {
+		this.product_day_hotel = product_day_hotel;
+	}
+}
